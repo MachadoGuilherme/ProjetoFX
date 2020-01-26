28c685d45a0414525e9cda860fc04e2b53726c7c
MachadoGuilherme <guilhermem81@gmail.com>
2020-01-26 13:56:17 +0000

    26/01/2020 - 13:56

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,23 @@
 package sample;
 
+import Dados.DadosPessoa;
+
+import Search.searchPessoa;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public abstract class Controller implements Initializable {
+
+    boolean modoEntrada;
 
     @FXML
     private Button botaoAdicionaSalvar;
@@ -59,9 +66,31 @@ public abstract class Controller implements Initializable {
     private Label Email;
 
     @FXML
-    private void botaoConConsult(ActionEvent event) {
+    private void botaoConConsult(ActionEvent event) throws ClassNotFoundException, SQLException {
 
+        if (consultaId.getText().trim().equals("")){
+            mensagemInformacao("Consulta", "Preencha o ID", "O ID Deve Ser Numerico!");
+        }
+        else {
+            try {
+                DadosPessoa p = searchPessoa.consultaPessoa(Integer.parseInt(consultaId.getText()));
+
+                if (p != null){
+                    preencheDados(p);
+                    modoEntrada = true;
+                }
+                else {
+                    mensagemInformacao("Consulta", "ID Invalido ou Inexistente!", "T");
+                }
+
+            }catch (SQLException e){
+
+                modoEntrada = false;
+                throw e;
+            }
+        }
     }
+
     @FXML
     private void botaoConLimpar(ActionEvent event) {
         consultaId.clear();
@@ -104,6 +133,22 @@ public abstract class Controller implements Initializable {
     //=================================================================================================================
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    //=================================================================================================================
+    private void mensagemInformacao(String titulo, String cabecalho, String texto) {
+        Alert alert = new Alert (Alert.AlertType.INFORMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(cabecalho);
+        alert.setContentText(texto);
+        alert.show();
+    }
+    //=================================================================================================================
+    private void preencheDados(DadosPessoa p ) {
+
+        consultaId.setText(String.valueOf(p.getIdPessoa()));
+        consultaNome.setText(p.getNomePessoa());
+        consultaEmail.setText(p.getEmailPessoa());
 
     }
     //=================================================================================================================
